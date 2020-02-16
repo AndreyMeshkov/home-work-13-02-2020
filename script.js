a9c742bf6ef5bcc458e108de9b2d18eb1c1d1a44
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-02-16 12:25:57 +0300

    add to create squares

---
+++ script.js
@@ -0,0 +1,21 @@
+var firstElem = document.createElement('div');
+firstElem.className = 'first';
+firstElem.style.backgroundColor = 'blue';
+firstElem.style.height = '50px';
+firstElem.style.width = '50px';
+document.body.append(firstElem);
+
+var secondElem= document.createElement('div');
+secondElem.className = 'second';
+secondElem.style.backgroundColor = 'red';
+secondElem.style.height = '70px';
+secondElem.style.width = '70px';
+document.body.append(secondElem);
+
+var thirdElem = document.createElement('div');
+thirdElem.className = 'third';
+thirdElem.style.backgroundColor = 'green';
+thirdElem.style.height = '90px';
+thirdElem.style.width = '90px';
+document.body.append(thirdElem);
+
