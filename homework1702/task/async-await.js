39e81bd7770194770993a4e79b07d58b0e7b970f
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-02-21 11:40:20 +0300

    fix: change asinc/await

--- homework1702/task/async-await.js
+++ homework1702/task/async-await.js
@@ -1,57 +1,13 @@
-const axios = require('axios'');
-
-const exchangeRate = euro * rate[toCurrency];
-const getExchangeRate = async (fromCurrency, toCurrency) => {
-  const response = await axios.get('http://data.fixer.io/api/latest?    access_key=[yourAccessKey]&format=1');
-  const rate = response.data.rates;
-  const euro = 1 / rate[fromCurrency];
-  const exchangeRate = euro * rate[toCurrency];
-  return exchangeRate;
-}
-const getCountries = async (currencyCode) => {}
-const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-return response.data.map(country => country.name);
-}
-const convert = async (fromCurrency, toCurrency, amount) => {
-  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
-  const countries = await getCountries(toCurrency);
-  const convertedAmount = (amount * exchangeRate).toFixed(2);
-  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
-}
-const getExchangeRate = async (fromCurrency, toCurrency) => {
-  try {
-    const response = await       axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1');
-    const rate = response.data.rates;
-    const euro = 1 / rate[fromCurrency];
-    const exchangeRate = euro * rate[toCurrency];
-    return exchangeRate;
-  } catch (error) {
-    throw new Error(`Unable to get currency ${fromCurrency} and  ${toCurrency}`);
-  }
-};
-
-const getCountries = async (currencyCode) => {
-  try {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-return response.data.map(country => country.name);
-  } catch (error) {
-    throw new Error(`Unable to get countries that use ${currencyCode}`);
-  }
-}
-
-convertCurrency('USD', 'HRK', 20)
-  .then((message) => {
-    console.log(message);
-  }).catch((error) => {
-    console.log(error.message);
+// Async/Await
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
   });
-
-
-
-
-
-
-
-
-
-
+}
+async function f1() {
+  var x = await resolveAfter2Seconds(10);
+  console.log(x); // 10
+}
+f1();
